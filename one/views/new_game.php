5775dfff32f74311ff2cc761d6a22dfd3ef073ff
Everthon Henrique <55612895+EverthonH@users.noreply.github.com>
2020-02-13 11:37:36 -0300

    Retirada dos bugs de inserir jogos

--- one/views/new_game.php
+++ one/views/new_game.php
@@ -1,7 +1,4 @@
 
-<?php if (!isset($_GET['editar'])) { ?>
-	
-
 <h1>Inserir um novo Jogo</h1>
 
 <form method="post" action="p_game.php">
@@ -17,25 +14,3 @@
 <br/>
 	<input type="submit" value="INSERIR JOGO">
 </form>
-
-<?php else { 
-	while ($dado = mysqli_fetch_array($consulta_jogos)) ?>
-
-
-<h1>Inserir um novo Jogo</h1>
-
-<form method="post" action="e_jogos.php">
-	<label>Nome do Jogo</label>
-	<br/>
-	<input type="text" name="NOME" placeholder="Editar o Nome do jogo">
-	<br>
-	<br/>
-	<label>Tipo do Jogo</label>
-	<br/>
-	<input type="text" name="TIPO" placeholder="Editar o Tipo do jogo">
-<br/>
-<br/>
-	<input type="submit" value="EDITAR JOGO">
-</form>
-
-<?php } ?> 
